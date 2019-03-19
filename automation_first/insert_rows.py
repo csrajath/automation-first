a05c13de7b2d5ed1feff47875b319e845a265a6b
rajath <rajathcs.1996@gmail.com>
2019-03-20 01:08:30 +0530

    folder name change

--- automation_first/insert_rows.py
+++
@@ -1,5 +0,0 @@
-'this python scripts automates the process of inserting N rows across sheets in a CSV file in one-go
-#!/usr/bin/env python3
-import pandas as pd 			#make sure pandas is installed (pip3 install pandas)
-def insert_rows_across_worksheets():	#function that inserts n rows in all the sheets present in a csv
-
