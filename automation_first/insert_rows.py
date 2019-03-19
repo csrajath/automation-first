23f5d8152ee06c4e567128b26a043ff9d75439a0
rajath <rajathcs.1996@gmail.com>
2019-03-20 00:47:50 +0530

    first automation

---
+++ automation_first/insert_rows.py
@@ -0,0 +1,5 @@
+'this python scripts enables
+#!/usr/bin/env python3
+import pandas as pd 			#make sure pandas is installed (pip3 install pandas)
+def insert_rows_across_worksheets():	#function that inserts n rows in all the sheets present in a csv
+
